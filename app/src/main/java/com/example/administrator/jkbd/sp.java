38a2e745d7356c7ad7f263f1cf04226c7ea54722
LIU8233 <842616355@qq.com>
2017-06-28 11:28:19 +0800

    编写闪屏代码

---
+++ app/src/main/java/com/example/administrator/jkbd/sp.java
@@ -0,0 +1,33 @@
+package com.example.administrator.jkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class sp extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp);
+        mCountDownTimer.start();
+    }
+    CountDownTimer mCountDownTimer=new CountDownTimer(3000,1000){
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent=new Intent(sp.this,MainActivity.class);
+            startActivity(intent);
+
+        }
+    };
+}
