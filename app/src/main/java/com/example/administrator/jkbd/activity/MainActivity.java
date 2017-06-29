048f2bf6f372a4c4a5f321c39dbd649835913d7a
LIU8233 <842616355@qq.com>
2017-06-29 22:40:44 +0800

    设置题目的路径

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.administrator.jkbd.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.Testtime;
+import com.example.administrator.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        OkHttpUtils<Testtime> utils=new OkHttpUtils<>(getApplicationContext());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(uri).targetClass(Testtime.class)
+                .execute(new OkHttpUtils.OnCompleteListener<Testtime>(){
+                    @Override
+                    public void onSuccess(Testtime result) {
+                        Log.e("main","result"+result);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error"+error);
+
+                    }
+                });
         startActivity(new Intent(MainActivity.this,testActivity.class));
     }
 
