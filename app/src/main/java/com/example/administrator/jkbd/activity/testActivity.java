fb1919694bdb903cbad941ed4f9cb9cd40cd7a02
LIU8233 <842616355@qq.com>
2017-06-30 16:57:13 +0800

    显示题目

--- app/src/main/java/com/example/administrator/jkbd/activity/testActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/testActivity.java
@@ -8,14 +8,17 @@ import android.widget.TextView;
 
 import com.example.administrator.jkbd.ExamApplication;
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.Question;
 import com.example.administrator.jkbd.bean.Testtime;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/6/29.
  */
 
 public class testActivity extends AppCompatActivity {
-    TextView tvInfo;
+    TextView tvInfo,tvExamTitle,tv1,tv2,tv3,tv4;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +29,28 @@ public class testActivity extends AppCompatActivity {
 
     private void initView() {
         tvInfo= (TextView) findViewById(R.id.tv_testInfo);
+        tvExamTitle= (TextView) findViewById(R.id.tv_question_title);
+        tvInfo= (TextView) findViewById(R.id.tv_answer_1);
+        tvInfo= (TextView) findViewById(R.id.tv_answer_2);
+        tvInfo= (TextView) findViewById(R.id.tv_answer_3);
+        tvInfo= (TextView) findViewById(R.id.tv_answer_4);
     }
 
     private void initData() {
         Testtime testtime = ExamApplication.getInstance().getmTesttime();
         if (testtime!=null){
             showData(testtime);
+        }
+        List<Question> examList=ExamApplication.getInstance().getMquestion();
+        if (examList!=null){
+            showExam(examList);
+        }
+    }
+
+    private void showExam(List<Question> examList) {
+        Question exam=examList.get(0);
+        if (exam!=null){
+            tvExamTitle.setText(exam.getQuestion());
         }
     }
 
