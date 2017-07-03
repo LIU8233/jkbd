4eaea4cb5190a982857e3b2dbdda184d5558ce7f
LIU8233 <842616355@qq.com>
2017-07-03 11:14:59 +0800

    分包，添加了biz和dao包

---
+++ app/src/main/java/com/example/administrator/jkbd/biz/IExamBiz.java
@@ -0,0 +1,10 @@
+package com.example.administrator.jkbd.biz;
+
+/**
+ * Created by Administrator on 2017/7/3.
+ */
+
+public interface IExamBiz {
+    void beginExam();
+    void nextQuestion();
+}
---
+++ app/src/main/java/com/example/administrator/jkbd/dao/IExamDao.java
@@ -0,0 +1,10 @@
+package com.example.administrator.jkbd.dao;
+
+/**
+ * Created by Administrator on 2017/7/3.
+ */
+
+public interface IExamDao {
+    void loadExamInfo();
+    void loadExamList();
+}
