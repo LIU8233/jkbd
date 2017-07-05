c3404e6868b71857e761eefc44f5d8375f0bf7e1
LIU8233 <842616355@qq.com>
2017-07-05 21:47:59 +0800

    做选择选项时画廊跟着变化

--- app/src/main/java/com/example/administrator/jkbd/activity/testActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/testActivity.java
@@ -271,9 +271,12 @@ public class testActivity extends AppCompatActivity {
         for (int i = 0; i < cbs.length ; i++) {
             if (cbs[i].isChecked()){
                 biz.getExam().setUserAnswer(String.valueOf(i+1));
+                aAdapter.notifyDataSetChanged();
                 return;
             }
         }
+        biz.getExam().setUserAnswer("");
+        aAdapter.notifyDataSetChanged();
 
     }
 
--- app/src/main/java/com/example/administrator/jkbd/view/QuestionAdapter.java
+++ app/src/main/java/com/example/administrator/jkbd/view/QuestionAdapter.java
@@ -1,6 +1,7 @@
 package com.example.administrator.jkbd.view;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -46,6 +47,13 @@ public class QuestionAdapter extends BaseAdapter {
         View view=View.inflate(mContext, R.layout.item_question,null);
         TextView tvNum= (TextView) view.findViewById(R.id.tv_num);
         ImageView ivQuestion= (ImageView) view.findViewById(R.id.iv_question);
+
+        String ua=mquestion.get(position).getUserAnswer();
+        if (ua!=null&&!ua.equals("")){
+            ivQuestion.setImageResource(R.mipmap.answer24x24);
+        }else {
+            ivQuestion.setImageResource(R.mipmap.ques24x24);
+        }
         tvNum.setText("第"+(position+1)+"题");
         return view;
     }
